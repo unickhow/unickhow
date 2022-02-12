1d26d4b04b6e4fba0bfd511d4968961d5cccccc5
unick_how <unick.zhow@gmail.com>
2022-02-12 15:59:55 +0800

    🧹 chore: vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
+<template>
+  <router-link :to="{ name: 'Home' }">Home</router-link>
+  <router-link :to="{ name: 'About' }">About</router-link>
+  <router-view></router-view>
+</template>
+
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
 
 <style>
 </style>
---
+++ src/pages/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>About Index</h1>
+  </main>
+</template>
+
+<script setup></script>
