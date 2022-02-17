276b3de855269973316fbcc673cbfbf423d2c95b
unick_how <unick.zhow@gmail.com>
2022-02-18 01:33:56 +0800

    🧹 chore: install unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import path from 'path'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(),
+    WindiCSS(),
+    Icons({ compiler: 'vue3' })
+  ],
   resolve: {
     alias: {
       '@': `${path.resolve(__dirname, 'src')}/`
