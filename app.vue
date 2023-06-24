f3c88de264ef6961c38011efeefd573f0a4a80c1
unickhow <unick.zhow@gmail.com>
2023-06-25 01:57:34 +0800

    💡 feat: bg toc animation

--- app.vue
+++ app.vue
@@ -3,4 +3,16 @@
   <NuxtPage />
   <SpeedDial />
   <AppFooter />
+
+  <div class="fixed -z-1 tok">
+    <img src="/ouroboros.png" class="w-full h-auto" alt="">
+  </div>
 </template>
+
+<style scoped>
+@import '~/styles/_animation.css';
+.tok {
+  @apply w-120 -bottom-60 -left-60 sm:w-180 sm:-bottom-90 sm:-left-90;
+  animation: spinning 240s linear infinite, breathing 20s ease-in-out infinite;
+}
+</style>
