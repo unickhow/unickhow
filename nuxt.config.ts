a09388fcc8fa489488e62e6ea87fb167ae136f84
unickhow <unick.zhow@gmail.com>
2023-09-02 16:59:58 +0800

    🧹 chore: add hotjar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     head: {
       title: '<unickhow />',
       script: [
+        // Google Analytics
         {
           src: 'https://www.googletagmanager.com/gtag/js?id=G-0ERCV84DE1',
           async: true
@@ -19,12 +20,26 @@ export default defineNuxtConfig({
           `
         },
         {
+          // Clarity tracking code for my site
           innerHTML: `
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "f3zey4tjot");
+          `
+        },
+        {
+          // Hotjar Tracking Code for my site
+          innerHTML: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2135847,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `
         }
       ],
